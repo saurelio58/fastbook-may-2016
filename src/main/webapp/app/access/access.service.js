fe9be315472daf4c842b6e28bd946f5ae391d6e8
Alex Jackson <alexfaithjm@gmail.com>
2016-05-03 14:16:06 -0500

    blah

--- src/main/webapp/app/access/access.service.js
+++ src/main/webapp/app/access/access.service.js
@@ -9,21 +9,15 @@
 
     function AccessService($http) {
 
-<<<<<<< Updated upstream
-      this.register() {
-=======
       this.register = (user) => {
->>>>>>> Stashed changes
+
         return $http
           .post('./api/users', user)
           .then(response => response.data)
         };
 
-<<<<<<< Updated upstream
-      this.login() {
-=======
       this.login = (email, domain) => {
->>>>>>> Stashed changes
+
         return $http
           .get('./api/users/login')
           .then(response => response.data)
