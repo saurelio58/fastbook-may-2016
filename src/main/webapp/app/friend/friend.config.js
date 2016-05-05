480c8b7e8cdaf495e657cc13278f9ac8e5b09d1c
Steve Aurelio <saurelio58@gmail.com>
2016-05-04 21:08:10 -0500

    added console.log messages

--- src/main/webapp/app/friend/friend.config.js
+++ src/main/webapp/app/friend/friend.config.js
@@ -12,6 +12,8 @@
         .forEach(key => {
           $stateProvider
             .state(key, friendRoutes[key]);  //accesses each state object given the key and the object
+            console.log(key);
+            console.log(friendRoutes[key]);
         })
     }
 
